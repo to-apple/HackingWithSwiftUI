95dbadbf9e4e16b942655294ead8374ac1e5656d
Shimoorook <jyoon@esri.com>
2022-01-18 20:20:57 -0800

    Initial Commit

---
+++ HackingWithSwiftUI/HackingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUIApp.swift
+//  HackingWithSwiftUI
+//
+//  Created by Juhan Yoon on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
